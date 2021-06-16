a58e7fb64bcc9d6f6fa53a48bbd37448792d68b2
aj-1904 <jainayush827@gmail.com>
2021-06-16 12:02:33 +0530

    Reading data from Firebase-1

--- src/App.js
+++ src/App.js
@@ -1,35 +1,41 @@
 import React from "react";
 import Cart from "./Cart";
 import Navbar from "./Navbar";
+import firebase from 'firebase/app';
+
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      products: [
-        {
-          price: 999,
-          title: "Watch",
-          qty: 1,
-          img: "https://images.unsplash.com/photo-1524805444758-089113d48a6d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8d2F0Y2h8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=60",
-          id: 1,
-        },
-        {
-          price: 9999,
-          title: "Mobile Phone",
-          qty: 1,
-          img: "https://images.unsplash.com/photo-1567581935884-3349723552ca?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8bW9iaWxlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=60",
-          id: 2,
-        },
-        {
-          price: 19999,
-          title: "Laptop",
-          qty: 1,
-          img: "https://images.unsplash.com/photo-1515343480029-43cdfe6b6aae?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGxhcHRvcHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=60",
-          id: 3,
-        },
-      ],
+      products: [],
+      loading: true 
     };
+  }
+
+  componentDidMount () {
+    firebase
+      .firestore()
+      .collection('products')
+      .get()
+      .then((snapshot) => {
+        console.log(snapshot);
+
+        snapshot.docs.map((doc) => {
+          console.log(doc.data);
+        });
+
+        const products = snapshot.docs.map((doc) => {
+          const data = doc.data();
+
+          data['id'] = doc.id
+          return data
+        })
+        this.setState({
+          products,
+          loading: false
+        })
+      })
   }
   handleIncreaseQuantity = (product) => {
     console.log("Hey please increase the qty of ", product);
@@ -95,7 +101,7 @@ class App extends React.Component {
   };
 
   render() {
-    const { products } = this.state;
+    const { products, loading } = this.state;
     return (
       <div className="App">
         <Navbar count={this.getCartCount()} />
@@ -105,6 +111,7 @@ class App extends React.Component {
           onDecreaseQuantity={this.handleDecreaseQuantity}
           onDeleteProduct={this.handleDeleteProduct}
         />
+        {loading && <h1>Loading Products...</h1>}
         <div style={{ padding: 10, fontSize: 20 }}>
           TOTAL: {this.getCartTotal()}
         </div>
